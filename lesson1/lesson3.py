b17f3b753d1eb6ea1449468d85c8630768fe2160
kirashostak2013-pixel <kirashostak2013@gmail.com>
2025-10-23 17:51:18 +0200

    next lesson

---
+++ lesson1/lesson3.py
@@ -0,0 +1,65 @@
+#import math
+
+#result = ((math.cos(7) + (math.pi **3) + 3))/(((6 **4) + 12 + math.cos(4)))
+
+#print(result)
+
+#import math
+
+#result = ((math.cos(math.pi **3) - (math.sin(2)))/(math.cos(math.sin(8)) - (3 **2) + (math.pi)))
+
+#print(result)
+
+#import math
+
+#result = ((8 + (math.sin(math.pi)))/(32 **6) - 83 + math.cos(2))
+
+#print(result)
+
+#import math
+
+#result = ((math.cos(7) + (math.pi **3) + 3))/(((6 **4)) + 12 + math.cos(4)))
+
+#print(result)
+
+#import math
+
+#result = ((math.cos(math.sin(8 **3)) + (math.pi)/(83 **2) - (18 **2) - math.sin(math.pi **3,)))
+
+#print = (result)
+
+#import math
+
+#result = ((math.cos(math.pi **3) - math.sin(2)/(math.cos(math.sin(8))) - (3 **2) + (math.pi)))
+
+#print(result)
+
+#import math 
+
+#result = ((32 **2) + (6 **4) - (math.pi) + (2)/(math.pi) * (3) - (6 **2) (math.cos(math.pi **2)))
+
+#print(result)
+
+#import math
+
+#result = ((math.cos(8 **math.pi)/(6 **2) - 3 **4 + 8 * (math.pi)))
+
+#print(result)
+
+#import math
+
+#result = ((6 + 8 **2 + (math.cos(math.pi))/(math.pi) + 8 **2))
+
+#print(result)
+
+#import math
+
+#result = ((math.cos(7) + (math.pi **3) + (3)/(6 **4) + 12 - (math.cos **4)))
+
+#print(result)
+
+#import math
+
+#result = ((8 + (math.sin(math.pi))/(32 **6) - 83 + (math.cos 2)))
+
+#print(result)
---
+++ lesson1/lesson4.py
@@ -0,0 +1,22 @@
+#user_age = int(input("write your age "))
+
+#if user_age == 60: 
+ #   print("sorry, you're too old for this program")
+#elif user_age >= 12:
+ #   print("you can use this program")
+#else:
+#    print("you can not use this program")
+
+#name_user = str(input("write your name "))
+
+#if name_user == "Ivan" or name_user == "Kate":
+ #   print("LEAVE!!!")
+#else:
+  #  print("you can come in ")
+
+user_points = int(input("how many points do you have? "))
+
+if user_points > 6 and user_points < 16:
+    print("you have a normal grade")
+else:
+    print("you dont have a very good grade")
