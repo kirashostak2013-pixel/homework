87d6bed7cb83e15d4054bb7448e45f675ed4fe1e
kirashostak2013-pixel <kirashostak2013@gmail.com>
2025-12-11 17:21:07 +0100

    hello

---
+++ lesson1/homeworklesson10.py
@@ -0,0 +1,56 @@
+#user_number = int(input("write a number "))
+
+#if user_number > 0:
+ #       print("its positive ")
+    #elif user_number < 0:
+ #       print("its negative ")
+    #else:
+   #     print("its 0 ")
+
+
+#user_number1 = int(input("write your first number "))
+#user_number2 = int(input(" enter your second number "))
+
+#if user_number1 > user_number2:
+ #   print(user_number1)
+#else:
+#    print(user_number2)
+
+
+#user_age = int(input("write your age "))
+
+#if user_age > 18:
+#    print("you are an adult ")
+#else:
+#    print("you are underage ")
+
+
+#user_number1 = int(input("enter your first number "))
+#user_number2 = int(input("enter your second number "))
+#user_number3 = int(input("enter your third and last number "))
+
+#if user_number1 > user_number2 and user_number3:
+ #   print(user_number1)
+#if user_number2 > user_number1 and user_number3:
+  #  print(user_number2)
+#if user_number3 > user_number1 and user_number2:
+#    print(user_number3)
+
+
+#user_temperature = int(input("write the temperature "))
+
+#if user_temperature < 0:
+#    print("its cold ")
+#elif user_temperature > 20:
+ #   print("its hot ")
+#else:
+#    print("its fine ")
+
+
+#user_number = int(input("write your number "))
+
+#if user_number % 5 == 0:
+ #   print(user_number / 5)
+#else:
+ #   print("it cant devide with 5 ")
+
---
+++ lesson1/lesson11.py
@@ -0,0 +1,8 @@
+user_age = int(input("write your age "))
+
+if user_age < 13:
+    print("you're a child ")
+if user_age >= 13 and user_age <= 17:
+    print("you're a teen ")
+if user_age > 17:
+    print("you're ancient ")
